7d4d22c2afcc10ecf41740b9d40446cef1911448
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-15 23:55:02 +0430

    use post excerpt to show a preview of posts

--- components/PostList.js
+++ components/PostList.js
@@ -4,18 +4,15 @@ function ContentList({ posts }) {
   return (
     <div>
       <h1>My Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.postId}>
-            <Link href={`/posts/${post.postId}`} passHref>
-              <h4>{post.title}</h4>
-            </Link>
-            <section
-              dangerouslySetInnerHTML={{ __html: post.content }}
-            ></section>
-          </li>
-        ))}
-      </ul>
+      {posts.map((post) => (
+        <div key={post.postId}>
+          <Link href={`/posts/${post.postId}`} passHref>
+            <h4>{post.title}</h4>
+          </Link>
+          <section dangerouslySetInnerHTML={{ __html: post.excerpt }}></section>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
