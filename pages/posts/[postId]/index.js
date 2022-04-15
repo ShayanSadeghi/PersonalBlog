92f8d9ed258f46ee26983144dcc4638e48d1073e
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-04-16 00:38:49 +0430

    added blog posts dynamic page

--- pages/posts/[postId]/index.js
+++ pages/posts/[postId]/index.js
@@ -1,9 +1,37 @@
-function ContentDetail() {
+function ContentDetail({ post }) {
   return (
     <div>
-      <h3>A Test post</h3>
+      <h2>{post.title.rendered}</h2>
+      <article
+        dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+      ></article>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  const posts = await (
+    await fetch("http://localhost:8000/wp-json/wp/v2/posts")
+  ).json();
+
+  return {
+    fallback: false,
+    paths: posts.map((post) => `/posts/${post.id}`),
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch post
+  const postId = context.params.postId;
+  const post = await (
+    await fetch(`http://localhost:8000/wp-json/wp/v2/posts/${postId}`)
+  ).json();
+
+  return {
+    props: {
+      post,
+    },
+  };
 }
 
 export default ContentDetail;
