55090c82325bfcbd98f87c8a3b1399eb8b3b84ce
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-05-01 23:58:35 +0430

    added rewrite to proxy a url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async rewrites() {
+    return [
+      {
+        source: "/api/login",
+        destination: "http://localhost:8000/wp-json/jwt-auth/v1/token",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
