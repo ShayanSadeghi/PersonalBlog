ffe206a303f27f778db1226c972e12225a6535d1
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-05-14 23:11:42 +0430

    added modal

--- components/admin/SkillManager.js
+++ components/admin/SkillManager.js
@@ -1,25 +1,71 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import ModalEditor from "../ModalEditor";
 
 function SkillManager({ token, data }) {
+  const [modalDisplay, setModalDisplay] = useState("none");
+
+  const [skillData, setSkillData] = useState({
+    id: "",
+    position: "",
+    title: "",
+  });
+
+  const rowClickHandler = (id) => {
+    setModalDisplay("block");
+    data.forEach((skill) => {
+      if (skill.id == id) {
+        setSkillData(skill);
+      }
+    });
+  };
+
+  const modalCloseHandler = () => {
+    setModalDisplay("none");
+  };
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">position</th>
-          <th scope="col">title</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            <th scope="row">{item.id}</th>
-            <td>{item.position}</td>
-            <td>{item.title}</td>
+    <div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">position</th>
+            <th scope="col">title</th>
+            <th scope="col"></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id} onClick={() => rowClickHandler(item.id)}>
+              <th scope="row">{item.id}</th>
+              <td>{item.position}</td>
+              <td>{item.title}</td>
+              <td>
+                <button
+                  className="btn"
+                  onClick={() => removeHandler(message.id)}
+                >
+                  <FontAwesomeIcon
+                    icon={faTrashCan}
+                    size="lg"
+                    className=" text-danger"
+                  />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <ModalEditor
+        name="Skill"
+        data={skillData}
+        display={modalDisplay}
+        closeHandler={modalCloseHandler}
+      />
+    </div>
   );
 }
 
