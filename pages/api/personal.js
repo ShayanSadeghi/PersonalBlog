a0b5c27012b6d526a2b0223294a9f1842219e0cf
Shayan Sadeghi <ShayanSadeghi1996@gmail.com>
2022-05-17 03:16:16 +0430

    added put and make the file cleaner

--- pages/api/personal.js
+++ pages/api/personal.js
@@ -2,11 +2,20 @@ const db = require("../../models/index");
 import { protect } from "../../helpers/api/jwt-middleware";
 
 export default async function PersonalDataHandler(req, res) {
-  const { id } = req.query; // get message id because for delete endpoint
+  const { id } = req.query;
+  console.log(id);
 
   return new Promise((resolve, reject) => {
     db.sequelize.sync();
-    if (req.method == "POST") {
+    if (req.method == "GET") {
+      db.Personal.findAll()
+        .then((data) => {
+          res.status(200).json(data);
+        })
+        .catch((err) => {
+          res.status(500).send(err);
+        });
+    } else if (req.method == "POST") {
       const data = req.body;
       db.Personal.create(data)
         .then((data) => {
@@ -15,23 +24,24 @@ export default async function PersonalDataHandler(req, res) {
         .catch((err) => {
           res.status(400).send(err);
         });
-    } else if (req.method == "GET") {
-      db.Personal.findAll()
+    } else if (req.method == "PUT") {
+      protect(req, res);
+      db.Personal.update(req.body, { where: { id: req.body.id } })
         .then((data) => {
-          res.status(200).json(data);
+          res.status(201).json(data);
         })
         .catch((err) => {
-          res.status(500).send(err);
+          res.status(500).json(err);
         });
     } else if (req.method === "DELETE") {
       protect(req, res);
+      console.log("hey");
       db.Personal.destroy({ where: { id: id } })
         .then(() => {
           res.status(202).json({ message: "done!" });
-          res;
         })
         .catch((err) => {
-          console.log(err);
+          res.status(500).send(err);
         });
     }
     resolve();
